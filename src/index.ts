31e700d51d55a4c9b0437bc4be5f155d54679f5d
Qwx <qwxmail@gmail.com>
2020-05-16 15:30:16 +0300

    Implemented basic subscriptions

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import Discord from 'discord.js';
 import { Token } from './config';
 import isStartMessage from './messageFilters/isStartMessage';
 import isStopMessage from './messageFilters/isStopMessage';
+import ggScraper from './ggScraper';
+import memory from './memory';
 
-import { ggScraper } from './ggScraper';
 
 
 const client = new Discord.Client();
@@ -18,31 +19,49 @@ client.once('ready', () => {
 });
 
 ggScraper.on('freebies-fetched', (urls: string[]): void => {
-    oldFreebies = urls;
+    memory.memorizeLinks(urls);
 })
+
 
 
 client.login(Token);
 
 ggScraper.fetchFreebies();
 
+let subscriptions: Map<string, Subscription> = new Map();
 
+setInterval(ggScraper.fetchFreebies, 1000 * 60 * 30);
 
 client.on('message', message => {
     if (message.mentions.users.first()?.id === geraldId) {
         console.log(message.toJSON())
 
         if (isStartMessage({ content: message.content })) {
-
-            message.channel.send("Starting spam!");
-            message.channel.send(oldFreebies[0])
+            if (subscriptions.has(message.channel.id)) {
+                message.channel.send("Already spammin'...");
+            } else {
+                const sendToChannel = (link: string): void => {
+                    message.channel.send(link);
+                };
+                memory.on('new-link-found', sendToChannel)
+                subscriptions.set(message.channel.id, sendToChannel);
+                message.channel.send('Starting spam!');
+            }
         }
 
         if (isStopMessage({ content: message.content })) {
-            message.channel.send("ok...");
+            if (subscriptions.has(message.channel.id)) {
+                message.channel.send('ok...');
+                const listener = subscriptions.get(message.channel.id) as Subscription;
+                memory.off('new-link-found', listener);
+            } else {
+                message.channel.send("I'm not doing anythin...");
+            }
         }
     }
 
 });
 
-
+interface Subscription {
+    (link: string): void;
+}
