005df9a6c9bab76b776ab56982a2d3d825e308d4
Qwx <qwxmail@gmail.com>
2020-06-27 11:03:56 +0300

    Fixed persistance test coverage, re-enabled coverage

--- src/subscriptions/persistance.test.ts
+++ src/subscriptions/persistance.test.ts
@@ -1,14 +1,26 @@
-import { saveToFile, loadFromFile } from './persistance'
-jest.mock('../config', () => { return { SubsFile: 'subs-test.txt' } });
-
 describe('persistane', () => {
+    afterEach(() => {
+        jest.resetModules();
+    })
 
     it('should save and load ids', () => {
-        const ids: string[] = ['123', 'abc'];
-        saveToFile(ids);
+        jest.doMock('../config', () => { return { SubsFile: 'subs-test.txt' } });
+        const persistance = require('./persistance');
 
-        const result = loadFromFile();
+        const ids: string[] = ['123', 'abc'];
+        persistance.saveToFile(ids);
+
+        const result = persistance.loadFromFile();
 
         expect(result).toEqual(ids);
+    })
+
+    it('should return empty array if subs file does not exist', () => {
+        jest.doMock('../config', () => { return { SubsFile: 'should-not-exist-subs-test.txt' } });
+        const persistance = require('./persistance');
+
+        const result = persistance.loadFromFile();
+
+        expect(result).toEqual([]);
     })
 });
