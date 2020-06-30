a9f161298fffcfa65f43a82e1901b564b13581bc
Qwx <qwxmail@gmail.com>
2020-07-01 00:42:55 +0300

    Mounted persistance wrapper to be used for subs

--- src/subscriptions/index.ts
+++ src/subscriptions/index.ts
@@ -2,16 +2,17 @@ import { SubscriptionManager } from "./SubscriptionManager";
 import { saveToFile, loadFromFile } from "./persistance/file";
 import { Client } from "discord.js";
 import { fetchChannels } from "./fetchChannels";
+import { load, save } from "./persistance/persistance";
 
 
 
 export const initSubscrptionManager = async (client: Client): Promise<SubscriptionManager> => {
-    const ids = loadFromFile();
+    const ids = load();
     console.log('loaded channel ids: ' + ids)
     const channels = await fetchChannels(client, ids)
     const subscriptionManager = new SubscriptionManager(channels);
     subscriptionManager.on('subscriptions-changed', (ids: string[]) => {
-        saveToFile(ids);
+        save(ids);
     });
     return subscriptionManager;
 }
