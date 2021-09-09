2b5ef276d2029e99d0530a9e28a9a2b27c821c0e
SparrowBrain <qwxmail@gmail.com>
2021-09-09 01:22:37 +0000

    Added error handling for channel fetch.

--- src/subscriptions/fetchChannels.ts
+++ src/subscriptions/fetchChannels.ts
@@ -1,9 +1,20 @@
 import { DMChannel, Client, Channel, NewsChannel, TextChannel } from "discord.js"
 
 export const fetchChannels = async (client: Client, ids: string[]): Promise<(DMChannel | NewsChannel | TextChannel)[]> => {
-    const channels: (DMChannel | NewsChannel | TextChannel)[] = (await Promise.all(ids.map(async (id: string) =>
-        await client.channels.fetch(id)
+    const fetchedChannels: Channel[] = (await Promise.all(ids.map(async (id: string) => {
+        try {
+            return await client.channels.fetch(id);
+        }
+        catch (ex) {
+            console.error(ex);
+            console.log(`Error while fetching channel ${id}.`);
+            return null;
+        }
+    }
     )))
+    .filter((channel: Channel | null) => channel !== null) as Channel[];
+
+    const channels: (DMChannel | NewsChannel | TextChannel)[] = fetchedChannels
         .filter((channel: Channel) => channel.type === 'text' || channel.type === 'dm' || channel.type === 'news')
         .map((channel) => {
             if (channel.type === 'dm') {
