ccec323346c9cb6c131332e78974ac5bfeb0bcfb
Qwx <qwxmail@gmail.com>
2021-10-09 02:45:50 +0300

    fixed imports

--- src/debugSubsciptionsManager/DebugSubscriptionManager.ts
+++ src/debugSubsciptionsManager/DebugSubscriptionManager.ts
@@ -1,10 +1,10 @@
-import { Broadcast } from "./Broadcast";
-import { TextChannel, DMChannel, NewsChannel, Channel } from "discord.js";
-import { EventEmitter } from "events";
+import { TextChannel, DMChannel, NewsChannel, Channel } from 'discord.js'
+import { EventEmitter } from 'events'
+import { Broadcast } from '../shared/subscriptions/Broadcast'
 
 export declare interface DebugSubscriptionManager {
   on(
-    event: "debug-subscriptions-changed",
+    event: 'debug-subscriptions-changed',
     listener: (channelIds: string[]) => void
   ): this;
 }
@@ -12,50 +12,50 @@ export declare interface DebugSubscriptionManager {
 export class DebugSubscriptionManager extends EventEmitter {
   subscriptions: Map<string, Broadcast> = new Map();
 
-  constructor(channels: (DMChannel | NewsChannel | TextChannel)[]) {
-    super();
+  constructor (channels: (DMChannel | NewsChannel | TextChannel)[]) {
+    super()
 
     channels.forEach((channel: TextChannel | DMChannel | NewsChannel) =>
       this.addSubscription(channel)
-    );
+    )
   }
 
-  public broadcastMessage(message: string): void {
+  public broadcastMessage (message: string): void {
     this.subscriptions.forEach((send) => {
-      send(message);
-    });
+      send(message)
+    })
   }
 
-  public subscribe(channel: TextChannel | DMChannel | NewsChannel) {
+  public subscribe (channel: TextChannel | DMChannel | NewsChannel) {
     if (this.subscriptions.has(channel.id)) {
-      channel.send("Already spammin'...");
+      channel.send("Already spammin'...")
     } else {
-      this.addSubscription(channel);
-      channel.send("Debug messages started.");
+      this.addSubscription(channel)
+      channel.send('Debug messages started.')
     }
   }
 
-  public unsubscribe(channel: TextChannel | DMChannel | NewsChannel) {
+  public unsubscribe (channel: TextChannel | DMChannel | NewsChannel) {
     if (this.subscriptions.has(channel.id)) {
-      channel.send("stopping debug");
-      this.removeSubscription(channel);
+      channel.send('stopping debug')
+      this.removeSubscription(channel)
     } else {
-      channel.send("I'm not doing anythin'...");
+      channel.send("I'm not doing anythin'...")
     }
   }
 
-  private addSubscription(channel: DMChannel | TextChannel | NewsChannel) {
+  private addSubscription (channel: DMChannel | TextChannel | NewsChannel) {
     const sendToChannel = (link: string): void => {
-      channel.send(link);
-    };
-    this.subscriptions.set(channel.id, sendToChannel);
-    this.emit("debug-subscriptions-changed", [...this.subscriptions.keys()]);
-    console.log("debug subscription added to " + channel.id);
+      channel.send(link)
+    }
+    this.subscriptions.set(channel.id, sendToChannel)
+    this.emit('debug-subscriptions-changed', [...this.subscriptions.keys()])
+    console.log('debug subscription added to ' + channel.id)
   }
 
-  private removeSubscription(channel: DMChannel | TextChannel | NewsChannel) {
-    this.subscriptions.delete(channel.id);
-    this.emit("debug-subscriptions-changed", [...this.subscriptions.keys()]);
-    console.log("debug subscription removed from " + channel.id);
+  private removeSubscription (channel: DMChannel | TextChannel | NewsChannel) {
+    this.subscriptions.delete(channel.id)
+    this.emit('debug-subscriptions-changed', [...this.subscriptions.keys()])
+    console.log('debug subscription removed from ' + channel.id)
   }
 }
--- src/freebiesSubscriptions/FreebiesSubscriptionManager.ts
+++ src/freebiesSubscriptions/FreebiesSubscriptionManager.ts
@@ -1,10 +1,10 @@
-import { Broadcast } from "./Broadcast";
-import { TextChannel, DMChannel, NewsChannel, Channel } from "discord.js";
-import { EventEmitter } from "events";
+import { TextChannel, DMChannel, NewsChannel, Channel } from 'discord.js'
+import { EventEmitter } from 'events'
+import { Broadcast } from '../shared/subscriptions/Broadcast'
 
 export declare interface FreebiesSubscriptionManager {
   on(
-    event: "freebies-subscriptions-changed",
+    event: 'freebies-subscriptions-changed',
     listener: (channelIds: string[]) => void
   ): this;
 }
@@ -12,55 +12,55 @@ export declare interface FreebiesSubscriptionManager {
 export class FreebiesSubscriptionManager extends EventEmitter {
   subscriptions: Map<string, Broadcast> = new Map();
 
-  constructor(channels: (DMChannel | NewsChannel | TextChannel)[]) {
-    super();
+  constructor (channels: (DMChannel | NewsChannel | TextChannel)[]) {
+    super()
 
     channels.forEach((channel: TextChannel | DMChannel | NewsChannel) =>
       this.addSubscription(channel)
-    );
+    )
   }
 
-  public broadcastMessage(message: string): void {
+  public broadcastMessage (message: string): void {
     this.subscriptions.forEach((send) => {
-      send(message);
-    });
+      send(message)
+    })
   }
 
-  public subscribe(channel: TextChannel | DMChannel | NewsChannel) {
+  public subscribe (channel: TextChannel | DMChannel | NewsChannel) {
     if (this.subscriptions.has(channel.id)) {
-      channel.send("Already spammin'...");
+      channel.send("Already spammin'...")
     } else {
-      this.addSubscription(channel);
-      channel.send("Starting spam!");
+      this.addSubscription(channel)
+      channel.send('Starting spam!')
     }
   }
 
-  public unsubscribe(channel: TextChannel | DMChannel | NewsChannel) {
+  public unsubscribe (channel: TextChannel | DMChannel | NewsChannel) {
     if (this.subscriptions.has(channel.id)) {
-      channel.send("ok...");
-      this.removeSubscription(channel);
+      channel.send('ok...')
+      this.removeSubscription(channel)
     } else {
-      channel.send("I'm not doing anythin'...");
+      channel.send("I'm not doing anythin'...")
     }
   }
 
-  public tellSubsNumber(channel: TextChannel | DMChannel | NewsChannel) {
-    const subsCount = this.subscriptions.size;
-    channel.send("Currently I have " + subsCount + "freebies subscribers.");
+  public tellSubsNumber (channel: TextChannel | DMChannel | NewsChannel) {
+    const subsCount = this.subscriptions.size
+    channel.send('Currently I have ' + subsCount + 'freebies subscribers.')
   }
 
-  private addSubscription(channel: DMChannel | TextChannel | NewsChannel) {
+  private addSubscription (channel: DMChannel | TextChannel | NewsChannel) {
     const sendToChannel = (link: string): void => {
-      channel.send(link);
-    };
-    this.subscriptions.set(channel.id, sendToChannel);
-    this.emit("freebies-subscriptions-changed", [...this.subscriptions.keys()]);
-    console.log("freebies subscription added to " + channel.id);
+      channel.send(link)
+    }
+    this.subscriptions.set(channel.id, sendToChannel)
+    this.emit('freebies-subscriptions-changed', [...this.subscriptions.keys()])
+    console.log('freebies subscription added to ' + channel.id)
   }
 
-  private removeSubscription(channel: DMChannel | TextChannel | NewsChannel) {
-    this.subscriptions.delete(channel.id);
-    this.emit("freebies-subscriptions-changed", [...this.subscriptions.keys()]);
-    console.log("freebies subscription removed from " + channel.id);
+  private removeSubscription (channel: DMChannel | TextChannel | NewsChannel) {
+    this.subscriptions.delete(channel.id)
+    this.emit('freebies-subscriptions-changed', [...this.subscriptions.keys()])
+    console.log('freebies subscription removed from ' + channel.id)
   }
 }
