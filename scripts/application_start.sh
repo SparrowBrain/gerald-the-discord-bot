168edef7b5adc9c4ad7d460ac92517db2d0c1f17
Qwx <qwxmail@gmail.com>
2022-02-12 19:15:53 +0200

    Moved waiting into validation

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/app
-port=$(aws ssm get-parameters --region eu-north-1 --names API_PORT --query Parameters[0].Value)
-API_PORT="${port%\"}"
-API_PORT="${API_PORT#\"}"
-
-npm start & npx wait-on -t 60000 http://localhost:$API_PORT/health
+npm start &
--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -2,4 +2,4 @@
 port=$(aws ssm get-parameters --region eu-north-1 --names API_PORT --query Parameters[0].Value)
 API_PORT="${port%\"}"
 API_PORT="${API_PORT#\"}"
-curl -X GET http:/localhost:${API_PORT}/health
+npx wait-on -t 120000 http://localhost:$API_PORT/health
