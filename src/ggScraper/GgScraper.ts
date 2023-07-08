f0303a06b40dc11b70c50e7a2838b6162b9dd572
SparrowBrain <qwxmail@gmail.com>
2023-07-08 16:08:30 +0300

    Added user agent to scraper

--- src/ggScraper/GgScraper.ts
+++ src/ggScraper/GgScraper.ts
@@ -1,5 +1,6 @@
 import https from 'https';
 import events from 'events';
+import fs from 'fs';
 import http, { IncomingMessage } from 'http';
 import { parsePage } from './parsePage';
 import { WEBSITE_DOMAIN } from './index';
@@ -10,8 +11,34 @@ export declare interface GgScraper {
 export class GgScraper extends events.EventEmitter {
   fetchFreebies () {
     const instance: GgScraper = this;
+
+    const userAgents = fs.readFileSync('src/ggScraper/userAgents.txt', 'utf-8').split('\r\n');
+    const userAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
+    console.log(`User-Agent: ${userAgent}`);
+
+    const url = new URL(WEBSITE_DOMAIN + '/news/freebies');
+
+    const httpsOptions: https.RequestOptions = {
+      protocol: url.protocol,
+      hostname: url.hostname,
+      port: url.port ?? 443,
+      path: url.pathname,
+      method: 'GET',
+      headers: {
+        'User-Agent': userAgent,
+        Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br'
+      }
+    };
+
+    const httpOptions: http.RequestOptions = {
+      ...httpsOptions,
+      port: url.port ?? 80
+    };
+
     const getFunction = WEBSITE_DOMAIN.startsWith('https') ? https.get : http.get;
-    getFunction(WEBSITE_DOMAIN + '/news/freebies', function (resp: IncomingMessage) {
+    getFunction(WEBSITE_DOMAIN.startsWith('https') ? httpsOptions : httpOptions, function (resp: IncomingMessage) {
       console.log('Got response: ' + resp.statusCode);
       let data = '';
       // A chunk of data has been recieved.
