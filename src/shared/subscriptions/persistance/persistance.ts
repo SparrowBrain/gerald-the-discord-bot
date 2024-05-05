6c129caa38812af4e0a8a80a83cc772e86b20346
SparrowBrain <qwxmail@gmail.com>
2024-05-05 05:49:54 +0000

    Remived s3

--- src/shared/subscriptions/persistance/persistance.ts
+++ src/shared/subscriptions/persistance/persistance.ts
@@ -1,5 +1,4 @@
 import { saveToFile, loadFromFile } from './file';
-import { saveToS3, loadFromS3 } from './s3';
 import * as config from '../../../config';
 import { SubscriptionsProvider } from '../provider';
 
@@ -7,9 +6,6 @@ export const save = async (channelIds: string[], file: string): Promise<void> =>
   if (config.SubsProvider === SubscriptionsProvider.File) {
     console.log(`saving subs to local file ${file}`);
     saveToFile(channelIds, file);
-  } else if (config.SubsProvider === SubscriptionsProvider.S3) {
-    console.log(`saving subs to s3 ${file}`);
-    await saveToS3(channelIds, file);
   } else {
     throw new Error('Invalid subscriptions provider configuraiton');
   }
@@ -19,9 +15,6 @@ export const load = async (file: string): Promise<string[]> => {
   if (config.SubsProvider === SubscriptionsProvider.File) {
     console.log('loading subs from local file ' + file);
     return loadFromFile(file);
-  } else if (config.SubsProvider === SubscriptionsProvider.S3) {
-    console.log('loading subs from s3 ' + file);
-    return await loadFromS3(file);
   } else {
     throw new Error('Invalid subscriptions provider configuraiton');
   }
